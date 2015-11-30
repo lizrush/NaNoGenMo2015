1c613c422402301ed3396b0695084c5b8029a199
Rush <lizr@algorithmia.com>
2015-11-30 10:59:37 -0800

    organize

---
+++ scripts/generate_trigrams.py
@@ -0,0 +1,29 @@
+import Algorithmia
+import os
+
+client             = Algorithmia.client('simG4c7kU+Seay4VpjAP3MSovuR1')
+sentence_split     = client.algo('StanfordNLP/SentenceSplit/0.1.0')
+generate_trigrams  = client.algo('ngram/GenerateTrigramFrequencies/0.1.1')
+
+corpus  = []
+rootdir = './clean_books/'
+book = ''
+
+for subdir, dirs, files in os.walk(rootdir):
+  for filename in files:
+    with open('./clean_books/' + filename, 'r') as content_file:
+      input = content_file.read()
+      sentences = sentence_split.pipe(input)
+      corpus.extend(sentences)
+
+#  generate trigrams
+input = [corpus,
+        "xxBeGiN142xx",
+        "xxEnD142xx",
+        "data://.algo/ngram/GenerateTrigramFrequencies/temp/trigrams-for-generation.txt"]
+
+trigrams_file = generate_trigrams.pipe(input)
+
+print "Done!"
+print "Your trigrams file is now available on Algorithmia"
+
